1a28699539b81dfb00f6b770b2719dfaf78018c1
nileshzarkar <nileshzarkar@users.noreply.github.com>
2022-05-30 11:33:22 +0530

    First commit

---
+++ spring-boot-thymeleaf-part-2/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
