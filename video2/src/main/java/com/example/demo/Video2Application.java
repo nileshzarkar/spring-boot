e850ed221159a62a62127cab911457112e1e0be0
nileshzarkar <nileshzarkar@users.noreply.github.com>
2022-05-30 08:19:25 +0530

    First commit

---
+++ video2/src/main/java/com/example/demo/Video2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Video2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Video2Application.class, args);
+	}
+
+}
