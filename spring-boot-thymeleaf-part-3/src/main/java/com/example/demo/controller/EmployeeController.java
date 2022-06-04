1b88039ecdbe5fee279cb7ed9d08ff14345b9845
nileshzarkar <nileshzarkar@users.noreply.github.com>
2022-06-04 15:02:33 +0530

    First Commit

---
+++ spring-boot-thymeleaf-part-3/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model){
+        model.addAttribute("listEmployees", employeeService.getAllEmployees());
+        return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "new_employee";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save employee to database
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+
+}
